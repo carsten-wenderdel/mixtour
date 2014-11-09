3cde15e3bc7cc45970397b4647e8d58d999fb1d4
Carsten Wenderdel <public@wenderdel.com>
2014-11-09 20:41:01 +0100

    Made main view controller a swift class - no swift code so far

---
+++ mixtour/MIXSwiftViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MIXSwiftViewController.swift
+//  mixtour
+//
+//  Created by Wenderdel, Carsten on 09/11/14.
+//  Copyright (c) 2014 Carsten Wenderdel. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MIXSwiftViewController: MIXViewController {
+
+}
