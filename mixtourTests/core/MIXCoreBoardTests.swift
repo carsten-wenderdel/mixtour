37123b2b6589c50bf2688bae16bd763fe63d2b82
Carsten Wenderdel <public@wenderdel.com>
2015-01-30 21:18:19 +0100

    Added strideofValue to sizeofValue in unit test to be safe

--- mixtourTests/core/MIXCoreBoardTests.swift
+++ mixtourTests/core/MIXCoreBoardTests.swift
@@ -11,11 +11,14 @@ import XCTest
 class MIXCoreBoardTests : XCTestCase {
     
     func testBoard() {
+        // Not sure about the difference between strideof and sizeof. Currently Swift documention is not clear enough about it. So better use both in unit test - 60 bytes for C struct!
         var varStruct : MIXCoreBoard = mixtour.createNonInitializedCoreBoard()
         XCTAssertEqual(60, sizeofValue(varStruct))
+        XCTAssertEqual(60, strideofValue(varStruct))
 
         let letStruct : MIXCoreBoard = mixtour.createNonInitializedCoreBoard()
         XCTAssertEqual(60, sizeofValue(letStruct))
+        XCTAssertEqual(60, strideofValue(letStruct))
     }
 }
 
