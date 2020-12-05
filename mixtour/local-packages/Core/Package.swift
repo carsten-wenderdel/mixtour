886cbfd2476dcc3e9fbc1937227af6719f4a9620
Carsten Wenderdel <public@wenderdel.com>
2020-12-05 23:28:30 +0100

    Remove Core unit tests

--- mixtour/local-packages/Core/Package.swift
+++ mixtour/local-packages/Core/Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "Core",
             dependencies: []),
-        .testTarget(
-            name: "CoreTests",
-            dependencies: ["Core"]),
     ]
 )
