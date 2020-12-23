810051a07a40e06fd3a9bd051cc143b45118f534
Carsten Wenderdel <public@wenderdel.com>
2020-12-23 16:55:42 +0100

    Add new target mixtour using SwiftUI

---
+++ mixtour/mixtourApp.swift
@@ -0,0 +1,18 @@
+//
+//  mixtourApp.swift
+//  mixtour
+//
+//  Created by Carsten Wenderdel on 23.12.20.
+//  Copyright © 2020 Carsten Wenderdel. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct mixtourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
