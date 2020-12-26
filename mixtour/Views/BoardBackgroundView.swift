f68d22cec36435e9e3457920cb1c643116a5dffa
Carsten Wenderdel <public@wenderdel.com>
2020-12-26 23:13:59 +0100

    Move board background to extra view

---
+++ mixtour/Views/BoardBackgroundView.swift
@@ -0,0 +1,29 @@
+import SwiftUI
+import MixModel
+
+struct BoardBackgroundView: View {
+    var body: some View {
+        GeometryReader { proxy in
+            VStack(spacing: 0) {
+                ForEach(0..<numberOfSquares) { line in
+                    HStack(spacing: 0) {
+                        ForEach(0..<numberOfSquares) { column in
+                            let color = (line + column) % 2 == 0
+                                ? Color(red: 0.9, green: 0.93, blue: 1.0)
+                                : Color(red: 0.8, green: 0.85, blue: 1.0)
+                            Rectangle()
+                                .foregroundColor(color)
+                        }
+                    }
+                }
+            }
+        }
+        .aspectRatio(1, contentMode: .fit)
+    }
+}
+
+struct BoardBackgroundView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardBackgroundView()
+    }
+}
--- mixtour/Views/GameBackgroundView.swift
+++ mixtour/Views/GameBackgroundView.swift
@@ -7,28 +7,23 @@ struct GameBackgroundView: View {
 
     var body: some View {
         GeometryReader { proxy in
-            VStack(spacing: 0) {
-                ForEach(0..<numberOfSquares) { line in
-                    HStack(spacing: 0) {
-                        ForEach(0..<numberOfSquares) { column in
-                            let color = (line + column) % 2 == 0
-                                ? Color(red: 0.9, green: 0.93, blue: 1.0)
-                                : Color(red: 0.8, green: 0.85, blue: 1.0)
-
-                            let square = ModelSquare(column: column, line: line)
-
-                            ZStack() {
-                                Rectangle()
-                                    .foregroundColor(color)
+            ZStack {
+                BoardBackgroundView()
+                VStack(spacing: 0) {
+                    ForEach(0..<numberOfSquares) { line in
+                        HStack(spacing: 0) {
+                            ForEach(0..<numberOfSquares) { column in
+                                let square = ModelSquare(column: column, line: line)
                                 PieceStackView(namespace: namespace, pieces: board.piecesAtSquare(square))
-                            }
-                            .zIndex(board.zIndexForColumn(column))
-                            .onTapGesture(count: 2) {
-                                board.trySettingPieceTo(square)
+                                    .zIndex(board.zIndexForColumn(column))
+                                    .contentShape(Rectangle()) // to allow taps on empty views
+                                    .onTapGesture(count: 2) {
+                                        board.trySettingPieceTo(square)
+                                    }
                             }
                         }
+                        .zIndex(board.zIndexForLine(line))
                     }
-                    .zIndex(board.zIndexForLine(line))
                 }
             }
         }
