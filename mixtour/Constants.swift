b79e02b867022f13db81781f0ae694adfab674c3
Carsten Wenderdel <public@wenderdel.com>
2021-01-09 22:00:32 +0100

    SF Images on buttons

---
+++ mixtour/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+import SwiftUI
+
+struct Constants {
+    static let buttonTextFont: Font = .system(size: 20, weight: .semibold)
+    static let buttonImageFont: Font = .system(size: 20, weight: .semibold)
+}
--- mixtour/Views/ContentView.swift
+++ mixtour/Views/ContentView.swift
@@ -10,25 +10,36 @@ struct ContentView: View {
                 .ignoresSafeArea()
 
             VStack {
+                Spacer()
+
                 HStack {
-                    Button("New Game") {
+                    Spacer()
+                    Button(action: {
                         board.reset()
+                    }) {
+                        Image(systemName: "plus")
+                            .font(Constants.buttonImageFont)
+                        Text("New Game")
+                            .font(Constants.buttonTextFont)
                     }
 
                     Spacer()
-
-                    Button("Undo") {
+                    Button(action: {
                         board.undo()
+                    }) {
+                        Image(systemName: "arrow.uturn.backward")
+                            .font(Constants.buttonImageFont)
+                        Text("Undo")
+                            .font(Constants.buttonTextFont)
                     }
                     .disabled(!board.undoPossible)
 
                     Spacer()
-
-                    Text(board.gameOverText)
                 }
+                Spacer()
+                Text(board.gameOverText)
 
                 Spacer()
-
                 GameView(board: board)
                     .disabled(board.interactionDisabled)
 
@@ -41,6 +52,7 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewLayout(.fixed(width: 667, height: 375))
+            .environment(\.colorScheme, .dark)
+//            .previewLayout(.fixed(width: 667, height: 375))
     }
 }
