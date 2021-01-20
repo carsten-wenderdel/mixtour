804530413a3096244ad22ed9464f2c5a9f6167e1
Carsten Wenderdel <public@wenderdel.com>
2021-01-20 20:02:23 +0100

    Remove obsolete C ComputerPlayer code

--- local-packages/Core/Sources/Core/include/Core.h
+++ local-packages/Core/Sources/Core/include/Core.h
@@ -1,7 +1,6 @@
 // public headers
 #import "../MIXCore.h"
 #import "../MIXCoreBoard.h"
-#import "../MIXCoreBoard_AI.h"
 
 // Internal headers for unit testing only
 #import "../MIXCoreBoard_private.h"
