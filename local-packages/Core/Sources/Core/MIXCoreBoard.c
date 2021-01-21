52c904d53352e3edc64ad41db1e4bc8f1379f9c7
Carsten Wenderdel <public@wenderdel.com>
2021-01-21 23:16:02 +0100

    Again increase performance by 7 or 8 percent. Less time spent in some for loops, one if-clause removed, replaced two multiplications with additions.

--- local-packages/Core/Sources/Core/MIXCoreBoard.c
+++ local-packages/Core/Sources/Core/MIXCoreBoard.c
@@ -94,7 +94,7 @@ uint8_t heightOfSquare(MIXCoreBoardRef boardRef, MIXCoreSquare square) {
 
 MIXCorePlayer colorOfSquareAtPosition(MIXCoreBoardRef boardRef, MIXCoreSquare square, uint8_t position) {
     
-    // All colors at that position encoded in on integer variable:
+    // All colors at that position encoded in one integer variable:
     uint8_t color = boardRef->colors[square.column][square.line];
     // Shift colors to the right so that the important bit is at the very right:
     color >>= position;
@@ -329,8 +329,8 @@ void arrayOfLegalMoves(MIXCoreBoardRef boardRef, MIXMoveArray *moveArray) {
     MIXCorePlayer player = playerOnTurn(boardRef);
     bool playerHasPiecesLeft = numberOfPiecesForPlayer(boardRef, player) > 0;
     MIXCoreMove lastResortMove = MIXCoreMoveNoMove; // If no other move is found, this will be returned
-    for (int8_t i = 0; i < LENGTH_OF_BOARD; i++) {
-        for (int8_t j = 0; j < LENGTH_OF_BOARD; j++) {
+    for (int8_t i = 0; i < LENGTH_OF_BOARD; i++) {  // index for column to look at
+        for (int8_t j = 0; j < LENGTH_OF_BOARD; j++) { // index for line to look at
             MIXCoreSquare square = {i, j};
             uint8_t height = heightOfSquare(boardRef, square);
             if (height == 0) {
@@ -339,14 +339,40 @@ void arrayOfLegalMoves(MIXCoreBoardRef boardRef, MIXMoveArray *moveArray) {
                     kv_push(MIXCoreMove, *moveArray, move);
                 }
             } else { // try dragging
-                int8_t leftSignum = (i-height >= 0) ? -1 : 0;   // -1 means we could move from the left
+                // We know calculate the position of squares that have the right distance
+                // to the target square. Distance needs to be height of target square.
+                // There are up to eight directions to look at (up, down, left, right
+                // and four diagonals).
+                // Some directions we don't want to look at because it's out of the
+                // bounds of the board. So let's calculate that.
+                int8_t leftSignum;
+                int8_t leftColumn;
+                if (i - height >= 0) {
+                    // Left to the to-Square is enough board space to look for from-squares.
+                    leftSignum = -1;
+                    leftColumn = i - height;
+                } else {
+                    leftSignum = 0;
+                    leftColumn = i;
+                }
                 int8_t rightSignum = (i+height <= MIX_BIGGEST_BOARD_INDEX) ? 1 : 0;
-                int8_t topSignum = (j-height >= 0) ? -1 : 0;
+                int8_t topSignum;
+                int8_t topLine;
+                if (j - height >= 0) {
+                    // Above the to-Square is enough board space to look for from-squares.
+                    topSignum = -1;
+                    topLine = j - height;
+                } else {
+                    topSignum = 0;
+                    topLine = j;
+                }
                 int8_t bottomSignum = (j+height <= MIX_BIGGEST_BOARD_INDEX) ? 1 : 0;
-                for (int8_t columnSignum = leftSignum; columnSignum <= rightSignum; columnSignum++) {
-                    for (int8_t lineSignum = topSignum; lineSignum <= bottomSignum; lineSignum++) {
+                int8_t column = leftColumn;
+                for (int8_t columnSignum = leftSignum; columnSignum <= rightSignum; columnSignum++, column += height) {
+                    int8_t line = topLine;
+                    for (int8_t lineSignum = topSignum; lineSignum <= bottomSignum; lineSignum++, line += height) {
                         if (columnSignum != 0 || lineSignum != 0) { // don't look at orignal square
-                            MIXCoreSquare sourceSquare = {(uint8_t)(i + height*columnSignum), (uint8_t)(j + height*lineSignum)};
+                            MIXCoreSquare sourceSquare = {column, line};
                             if (!isSquareEmpty(boardRef, sourceSquare)) {
                                 // So we can drag as long nothing is between. Check that:
                                 if (!isSomethingBetweenSquares(boardRef, square, sourceSquare, columnSignum, lineSignum)) {
