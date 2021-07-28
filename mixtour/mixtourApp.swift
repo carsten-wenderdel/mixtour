659b76ba5849118de920a28f5415d426dc0a5061
Carsten Wenderdel <public@wenderdel.com>
2021-07-28 12:17:04 +0200

    Add Tab Bar

--- mixtour/mixtourApp.swift
+++ mixtour/mixtourApp.swift
@@ -4,7 +4,22 @@ import SwiftUI
 struct mixtourApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                ContentView()
+                    .tabItem {
+                        Label("Game", systemImage: "checkerboard.rectangle")
+//                        Label("Game", systemImage: "checkerboard.shield")
+                    }
+                ContentView()
+                    .tabItem {
+                        Label("Rules", systemImage: "list.bullet.rectangle.portrait.fill")
+//                        Label("Rules", systemImage: "list.bullet")
+                    }
+                ContentView()
+                    .tabItem {
+                        Label("Legal", systemImage: "info.circle.fill")
+                    }
+            }
         }
     }
 }
