d556c57ecfd331e904a6861a652ebaf5466e8706
Carsten Wenderdel <public@wenderdel.com>
2021-08-15 20:45:08 +0200

    New player strength configs (up to 3420 iterations)

--- local-packages/MixModel/Sources/MixModel/ComputerPlayer/MCPlayerConfig.swift
+++ local-packages/MixModel/Sources/MixModel/ComputerPlayer/MCPlayerConfig.swift
@@ -10,9 +10,11 @@ public enum MCPlayerConfig: Int, CaseIterable {
 
     case beginner1 = 45
     case beginner2 = 80
-    case advanced1 = 175
-    case advanced2 = 417
-    case expert1 = 1_000
+    case casualPlayer1 = 175
+    case casualPlayer2 = 417
+    case advanced1 = 1_000
+    case advanced2 = 1_935
+    case expert1 = 3_420
 
     public var eloRating: Int {
         switch self {
@@ -20,12 +22,16 @@ public enum MCPlayerConfig: Int, CaseIterable {
             return 1000
         case .beginner2:
             return 1150
-        case .advanced1:
+        case .casualPlayer1:
             return 1300
-        case .advanced2:
+        case .casualPlayer2:
             return 1450
-        case .expert1:
+        case .advanced1:
             return 1600
+        case .advanced2:
+            return 1750
+        case .expert1:
+            return 1900
         }
     }
 }
@@ -152,56 +158,81 @@ public enum MCPlayerConfig: Int, CaseIterable {
 //        combined: 73.74005305039788
 
 
-// When competing against 1000 iterations. So we picked 417
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 450)
-//        450.0 iterations
-//        combined: 69.55555555555556
+// Vs 1000 iterations reversed. So we picked 1935
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 440)
-//        1193.0 iterations
-//        combined: 69.55155071248952
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1850)
+//        2671.0 iterations
+//        combined: 67.62448521153127
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 430)
-//        756.0 iterations
-//        combined: 69.1137566137566
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1880)
+//        2787.0 iterations
+//        combined: 68.9989235737352
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 420)
-//        1014.0 iterations
-//        combined: 71.00591715976331
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1890)
+//        2507.0 iterations
+//        combined: 68.53809333865178
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 418)
-//        1011.0 iterations
-//        combined: 69.8813056379822
-//
-//        let player2 = MonteCarloPlayer(numberOfIterations: 417)
-//        2015.0 iterations
-//        combined: 70.03722084367246
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1900)
+//        2625.0 iterations
+//        combined: 70.67619047619047
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 416)
-//        2039.0 iterations
-//        combined: 71.44433545855811
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1920)
+//        2880.0 iterations
+//        combined: 70.13020833333333
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 415)
-//        1078.0 iterations
-//        combined: 69.68923933209648
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1940)
+//        2843.0 iterations
+//        combined: 69.85578614139993
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 414)
-//        2007.0 iterations
-//        combined: 71.52466367713005
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1950)
+//        2670.0 iterations
+//        combined: 70.72097378277154
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 413)
-//        1134.0 iterations
-//        combined: 71.05379188712521
+//        let player1 = MonteCarloPlayer(numberOfIterations: 1970)
+//        2522.0 iterations
+//        combined: 71.25297383029341
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 412)
-//        1023.0 iterations
-//        combined: 70.89442815249267
+//        let player1 = MonteCarloPlayer(numberOfIterations: 2000)
+//        1508.0 iterations
+//        combined: 71.93302387267904
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 410)
-//        775.0 iterations
-//        combined: 73.7741935483871
+//        let player1 = MonteCarloPlayer(numberOfIterations: 2200)
+//        467.0 iterations
+//        combined: 74.30406852248395
 
-//        let player2 = MonteCarloPlayer(numberOfIterations: 400)
-//        377.0 iterations
-//        combined: 73.74005305039788
+
+
+// VS 1935 (other way around). So we picked 3420
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3200)
+//        3217.0 iterations
+//        combined: 68.6897730805098
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3300)
+//        2116.0 iterations
+//        combined: 68.39555765595463
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3380)
+//        2411.0 iterations
+//        combined: 69.28660306926587
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3400)
+//        2265.0 iterations
+//        combined: 71.18101545253863
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3415)
+//        2562.0 iterations
+//        combined: 70.51131928181108
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3450)
+//        2031.0 iterations
+//        combined: 70.64254062038405
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3500)
+//        2095.0 iterations
+//        combined: 71.67064439140812
+
+//        let player1 = MonteCarloPlayer(numberOfIterations: 3800)
+//        2139.0 iterations
+//        combined: 73.35203366058906
--- mixtour/ViewModels/MCPlayerConfig+Strings.swift
+++ mixtour/ViewModels/MCPlayerConfig+Strings.swift
@@ -7,16 +7,20 @@ extension MCPlayerConfig {
         let name: String
         switch self {
         case .beginner1:
-            name = LocalizedString("Beginner 1")
+            name = "Beginner 1"
         case .beginner2:
-            name = LocalizedString("Beginner 2")
+            name = "Beginner 2"
+        case .casualPlayer1:
+            name = "Casual Player 1"
+        case .casualPlayer2:
+            name = "Casual Player 2"
         case .advanced1:
-            name = LocalizedString("Advanced 1")
+            name = "Advanced 1"
         case .advanced2:
-            name = LocalizedString("Advanced 2")
+            name = "Advanced 2"
         case .expert1:
-            name = LocalizedString("Expert 1")
+            name = "Expert 1"
         }
-        return "\(name) (ELO \(self.eloRating))"
+        return "\(LocalizedString(name)) (ELO \(self.eloRating))"
     }
 }
