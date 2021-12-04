9721de946710a9a6657bebddf6986c48ebab8ca9
Carsten Wenderdel <public@wenderdel.com>
2021-12-04 17:21:51 +0100

    Added target TabDemo

---
+++ TabDemo/TabDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  TabDemoApp.swift
+//  TabDemo
+//
+//  Created by Carsten Wenderdel on 04.12.21.
+//  Copyright © 2021 Carsten Wenderdel. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TabDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
