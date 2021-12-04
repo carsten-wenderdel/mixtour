2ab977b089786adca739a67c37c392a84bf324dc
Carsten Wenderdel <public@wenderdel.com>
2021-12-04 19:28:02 +0100

    Added TabDemo from other branch as extra target

--- TabDemo/TabDemoApp.swift
+++ TabDemo/TabDemoApp.swift
@@ -1,18 +1,76 @@
-//
-//  TabDemoApp.swift
-//  TabDemo
-//
-//  Created by Carsten Wenderdel on 04.12.21.
-//  Copyright © 2021 Carsten Wenderdel. All rights reserved.
-//
-
 import SwiftUI
 
 @main
-struct TabDemoApp: App {
+struct AnimationTestApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView {
+                BlaView()
+                    .tabItem {
+                        Text("Tab 1")
+                    }
+                Text("Content 2")
+                    .tabItem {
+                        Text("Tab 2")
+                    }
+            }
+        }
+    }
+}
+
+struct BlaView: View {
+    var body: some View {
+        ContentView()
+    }
+}
+
+struct ContentView: View {
+    @StateObject private var viewModel = ViewModel()
+    @Namespace var namespace
+
+    var body: some View {
+        VStack {
+            Button(action: {
+                viewModel.move()
+            }) {
+                Text("Move")
+            }
+
+            ZStack() {
+                Rectangle()
+                    .fill(.gray)
+                    .frame(width: 300, height: 300, alignment: .center)
+                if viewModel.moved {
+                    RectView()
+//                        .animation(Animation.default.speed(0.5))
+                        .matchedGeometryEffect(id: 0, in: namespace, properties: .frame)
+                        .offset(x: 0, y: -130)
+
+                } else {
+                    RectView()
+//                        .animation(Animation.default.speed(0.5))
+                        .matchedGeometryEffect(id: 0, in: namespace, properties: .frame)
+                        .offset(x: 0, y: 130)
+                }
+            }
+        }
+    }
+}
+
+struct RectView: View {
+    var body: some View {
+        Rectangle()
+            .frame(width: 100, height: 30, alignment: .center)
+    }
+}
+
+
+final class ViewModel: ObservableObject {
+    @Published var moved = false
+
+    func move() {
+        withAnimation(Animation.default.speed(0.5)) {
+            moved.toggle()
         }
     }
 }
