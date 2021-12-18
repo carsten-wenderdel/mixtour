a51f8ec64f23d29206c907c34e2c5649b798642a
Carsten Wenderdel <public@wenderdel.com>
2021-12-18 21:01:34 +0100

    Added Todo list to legal tab

--- mixtour/LegalTab/LegalView.swift
+++ mixtour/LegalTab/LegalView.swift
@@ -10,23 +10,41 @@ struct LegalView: View {
                 .ignoresSafeArea()
 
             VStack(spacing: 10) {
+                Group {
+                    Text("ToDo").font(.title)
+                    Text("Legal/Impressum")
+                    Text("Link to Spielstein")
+                    Text("German Rules")
+                    Text("Web page")
+                    Text("Privacy Web Page")
+                    Text("Privacy Statement")
+                    Text("App Icons")
+                }
+                Group {
+                    Text("Screenshots for App Store")
+                    Text("App Store Texts")
 
-                Text("Large Title").font(.largeTitle)
-                Text("Title").font(.title)
+                    Text("Known Bugs").font(.title)
+                    Text("Margin between board and unused pieces is different")
+                    Text("During Undo some pieces get hidden behind other pieces during movement")
+                }
 
-                Text("Some more Text 1, so long that it ")
-                    + Text("should wrap into multiple lines")
-                Text("Some more Text 2").font(.body)
-                Text("Title2").font(.title2).foregroundColor(.secondary)
-                Text("Title3").font(.title3).foregroundColor(.secondary)
-                Text("Headline").font(.headline)
-                Text("Subheadline").font(.subheadline)
-                Text("Callout").font(.callout)
-//                Text("Visit: ")
-//                + Text ("[www.spiegel.de](https://www.spiegel.de)")
-                Text("Caption: Tel: ")
-                + Text("[+49 123 456 789](tel:+49123456789)")
-                    .font(.caption)
+//                Text("Large Title").font(.largeTitle)
+//                Text("Title").font(.title)
+//
+//                Text("Some more Text 1, so long that it ")
+//                    + Text("should wrap into multiple lines")
+//                Text("Some more Text 2").font(.body)
+//                Text("Title2").font(.title2).foregroundColor(.secondary)
+//                Text("Title3").font(.title3).foregroundColor(.secondary)
+//                Text("Headline").font(.headline)
+//                Text("Subheadline").font(.subheadline)
+//                Text("Callout").font(.callout)
+////                Text("Visit: ")
+////                + Text ("[www.spiegel.de](https://www.spiegel.de)")
+//                Text("Caption: Tel: ")
+//                + Text("[+49 123 456 789](tel:+49123456789)")
+//                    .font(.caption)
             }
             .multilineTextAlignment(.center)
             .textSelection(.enabled)
